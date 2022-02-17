bc78845b315d4d56c5d89547f759242b5768f8ca
Spencer Evans-Cole <spencer.ec09@gmail.com>
2022-02-17 15:46:37 -0500

    Work on fetching upstream on request

--- resolver/parser.go
+++ resolver/parser.go
@@ -1,19 +1,23 @@
 package resolver
+
 import (
 	"github.com/miekg/dns"
 	//"log"
 	"net"
 )
-func Parser(message *dns.Msg) *dns.A{
- for _, msg := range message.Question{
-	 switch msg.Qclass{
-	 case dns.TypeA:
-			Query(msg)
-	 }
- }
- m :=dns.A{
-	Hdr: dns.RR_Header{Name: message.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 30, },
-	A: net.ParseIP("127.0.0.1"),
+
+func Parser(message *dns.Msg) *dns.A {
+	var ip string
+	for _, msg := range message.Question {
+		switch msg.Qclass {
+		case dns.TypeA:
+			
+			ip = Query(message)
+		}
+	}
+
+	return &dns.A{
+		Hdr: dns.RR_Header{Name: message.Question[0].Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60},
+		A:   net.ParseIP(ip),
+	}
 }
- return &m
-}	
--- resolver/query.go
+++ resolver/query.go
@@ -1,7 +1,21 @@
-package resolver 
+package resolver
+
 import (
+	"log"
+	
 	"github.com/miekg/dns"
 )
-func Query(domain dns.Question){
+func Query(domain *dns.Msg) string{
 	//domain.A
+//client := new(dns.Client)
+log.Println("Querying verizon for ", domain.Question[0].Name)
+	r, err := dns.Exchange(domain, "4.2.2.1:53")
+	log.Println("error: ",err.Error())
+	if err == nil{
+		if t, ok := r.Answer[0].(*dns.TXT); ok{
+			log.Println(t)
+			return t.Hdr.Name
+		}
+	}
+	return "127.0.0.1"
 }
