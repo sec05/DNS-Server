e45231da7720e258272da077ae039b8b02ab6721
Spencer Evans-Cole <spencer.ec09@gmail.com>
2022-03-23 10:42:17 -0400

    Testing timing

--- tests/TestDNS.py
+++ tests/TestDNS.py
@@ -1,5 +1,7 @@
+from random import random
 import dns.resolver
 import csv
+import time
 resolver = dns.resolver.Resolver(configure=False)
 resolver.nameservers = ['127.0.0.1']
 domains = []
@@ -8,7 +10,12 @@ with open(r'tests/domains.csv', 'r') as file:
     for row in reader:
         domains.append(row[1])
 domains.pop(0)
-for domain in domains:
-    answer = resolver.resolve(domain)
+start = time.time_ns()
+for i in range(len(domains)):
+    index = int(random() * len(domains))
+    answer = resolver.resolve(domains[index])
     for rr in answer:
-        print(domain +": "+str(rr))
+        print(domains[index] +": "+str(rr))
+    domains.pop(index)
+end = time.time_ns()
+print("Took "+str((end-start)/(10 ** 9))+" seconds to query the top 499 domains!")
