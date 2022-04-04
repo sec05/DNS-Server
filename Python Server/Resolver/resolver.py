e0d27f6ad3aca49cfd9fcd19cfd23d43a03d939c
Spencer Evans-Cole <spencer.ec09@gmail.com>
2022-04-04 14:27:26 -0400

    Work on python resolver

--- Python Server/Resolver/resolver.py
+++ Python Server/Resolver/resolver.py
@@ -1,4 +1,5 @@
 import dns.message
-
+import dns.rdtypes
 def ServeDNS(data: dns.message.Message):
-    print(data)
+    print(dns.rdtypes.IN.A)
+   # print(data.question[0].rdtype == dns.rdtypes.IN.A)
