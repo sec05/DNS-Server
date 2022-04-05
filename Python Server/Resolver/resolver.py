9c5723fe4f6e2f23dc7e363c47b8f83987abd041
sec05 <58738123+sec05@users.noreply.github.com>
2022-04-04 20:59:31 -0400

    rdtype equivalencies

--- Python Server/Resolver/resolver.py
+++ Python Server/Resolver/resolver.py
@@ -1,5 +1,12 @@
+from email.policy import default
+from soupsieve import match
 import dns.message
 import dns.rdtypes
 def ServeDNS(data: dns.message.Message):
-    print(dns.rdtypes.IN.A)
+    if(data.question[0].name == dns.rdtype.A):
+        
+    print(data.question[0].name)
+    print(data.question[0].rdclass)
+    print(data.question[0].rdtype == dns.rdatatype.A)
+    print(data.question[0].covers)
    # print(data.question[0].rdtype == dns.rdtypes.IN.A)
