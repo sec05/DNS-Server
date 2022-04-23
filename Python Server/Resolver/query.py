10840bc411dc35e72370075b89288be4423c94a0
Spencer Evans-Cole <spencer.ec09@gmail.com>
2022-04-23 10:00:14 -0400

    Querying fixes

--- Python Server/Resolver/query.py
+++ Python Server/Resolver/query.py
@@ -1,7 +1,27 @@
 import dns.message
 import dns.resolver
 import dns.rdatatype
+
 def Query(domain: str) -> dns.message.Message:
     q = dns.message.make_query(domain, dns.rdatatype.A, flags=0)
     r = dns.query.udp(q, "4.2.2.1")
+    
+    while len(r.answer) == 0:
+        print("looping")
+        i = 0
+        field = r.additional if len(r.additional) != 0 else r.authority
+        for d in field:
+            if d.rdtype == dns.rdatatype.A:
+                break
+            else:
+                i += 1
+        try:
+            r = dns.query.udp(q,field[i].to_rdataset().__getitem__(i).to_text())
+        except Exception as e:
+            print(e, field)
+            return r
+    while r.answer[0].rdtype == dns.rdatatype.CNAME:
+        q = dns.message.make_query(r.answer[0].to_rdataset().__getitem__(0).to_text(), dns.rdatatype.A, flags=0)
+        r = dns.query.udp(q, "4.2.2.1")
+        r.answer[0].name = domain
     return r
--- Python Server/Resolver/resolver.py
+++ Python Server/Resolver/resolver.py
@@ -3,11 +3,17 @@ import dns.rdatatype
 import dns.rrset
 import socket
 from Resolver.parser import Parser
+from Cache.Cache import Cache
+c = Cache(0)
 def ServeDNS(data: dns.message.Message, addr):
     
     message = dns.message.Message()
     message.question = data.question
-    name = Parser(message)
+    if c.Contains(message):
+          name = c.getItem(data.question[0].name)
+    else:
+      name = Parser(message)
+      c.add(data.question[0].name, name)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(("",53))
